0f0593b3137581543dbe1ddae170b3aa00ca52c5
Haqim Haslee <haqimhaslee26@gmail.com>
2023-02-19 02:58:45 +0800

    UPDATE ver23.2.672018292751 -NEW QuickAction func -ADDED new assets in Drawable (ANDROID ONLY for now)

--- lib/src/navbar.dart
+++ lib/src/navbar.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:quick_actions/quick_actions.dart';
+import 'package:utp_in_me/pages/digital_id.dart';
 import 'package:utp_in_me/pages/home.dart';
 import 'package:utp_in_me/pages/panic_button.dart';
 import 'package:utp_in_me/pages/more_app/more_app.dart';
-import 'package:utp_in_me/pages/srcutp/srcutp.dart';
+import 'package:utp_in_me/pages/shuttle_bus/shuttle_bus.dart';
+import 'package:utp_in_me/pages/ucs.dart';
+import 'package:utp_in_me/pages/ulearn.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,6 +16,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final quickActions = const QuickActions();
+
+  @override
+  void initState() {
+    super.initState();
+
+    quickActions.setShortcutItems(const [
+      ShortcutItem(type: 'ulearn', localizedTitle: 'ULearn', icon: 'ulearn'),
+      ShortcutItem(
+          type: 'digitalid', localizedTitle: 'Digital ID', icon: 'badge'),
+      ShortcutItem(
+          type: 'shuttlebus',
+          localizedTitle: 'Shuttle Bus',
+          icon: 'shuttlebus'),
+      ShortcutItem(type: 'ucs', localizedTitle: 'UCS', icon: 'ucs'),
+    ]);
+    quickActions.initialize((type) {
+      if (type == 'ulearn') {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const ULearn()));
+      } else if (type == 'digitalid') {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const DigitalId()));
+      } else if (type == 'shuttlebus') {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const ShuttleBus()));
+      } else if (type == 'ucs') {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const UCSPortal()));
+      }
+    });
+  }
+
   int _selectedIndex = 0;
   final List<Widget> _windgetOption = <Widget>[
     const Home(),
